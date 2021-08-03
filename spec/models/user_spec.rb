1524a813667677e55f29555c8e62862444999637
Anna Gabriel <yssgabriel@gmail.com>
2021-08-03 18:43:31 +0800

    Added user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
     expect(user).to be_valid
   end
+
+  it 'is not valid without username' do
+    user.username = ' '
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without password' do
+    user.password = '  '
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without firstname' do
+    user.firstname = ' '
+    expect(user).to_not be_valid
+  end
+
+  it 'has unique username' do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
 end
