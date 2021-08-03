971baee10d3e71dec0be2ce4ad8f691056b890d9
Anna Gabriel <yssgabriel@gmail.com>
2021-08-03 19:10:26 +0800

    Added user secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   validates :firstname, presence: true
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_095938) do
+ActiveRecord::Schema.define(version: 2021_08_03_105121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_095938) do
     t.string "username"
     t.string "firstname"
     t.string "lastname"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
