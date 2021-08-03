813266acf3d8fd1857d8f80cc9cd4831887315a6
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 01:18:29 +0800

    Redirect upon creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = 'Account was created successfully'
+      redirect_to @user
     end
   end
 
