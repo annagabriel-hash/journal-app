4c6596919cb4aba551f945e1239e64b8517dfeee
Anna Gabriel <yssgabriel@gmail.com>
2021-08-03 18:25:37 +0800

    Updated feature/user branch based on main branch

--- db/schema.rb
+++ db/schema.rb
@@ -31,5 +31,14 @@ ActiveRecord::Schema.define(version: 2021_08_04_122135) do
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
   add_foreign_key "tasks", "categories"
 end
