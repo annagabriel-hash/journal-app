51ced55fcc6f7847e8af2571878fa2754dc990d5
Anna Gabriel <yssgabriel@gmail.com>
2021-08-03 18:25:27 +0800

    Added user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {User.new(username: 'johndoe', firstname: 'John', lastname: 'Doe')}
+  let(:user) {User.new(username: 'johndoe', firstname: 'John', lastname: 'Doe', password: 'password')}
 
   it 'is valid with valid attributes' do
     expect(user).to be_valid
