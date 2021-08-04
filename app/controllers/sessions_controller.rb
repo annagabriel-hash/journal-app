e5a530b2d6115940dd4554b0d468a6f6c2d55b8d
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 17:17:56 +0800

    Refactor code to add local variable upon redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Login successful'
-      redirect_to user_path(user)
+      redirect_to user_path(user), notice: 'Login successful'
     else
       flash[:alert] = 'Invalid username or password'
       render :new
