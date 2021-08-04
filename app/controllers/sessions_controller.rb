141e16cc91e5374167eee535d829f2815435917f
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 15:11:30 +0800

    Update create action to search and save user id to session as per #8 Fix following errors: - Failure/Error: expect(session[:user_id]).to eq(user.id)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    session[:user_id] = user.id
   end
 end
