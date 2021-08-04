8d9a24c338e8c5cd48f2b59c130ef285acc63270
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 14:39:56 +0800

    Specify user logging in as per #7

---
+++ spec/system/users/login_users_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "LoginUsers", type: :system do
+  before do
+    driven_by(:rack_test)
+    user = User.create(username: 'johndoe', firstname: 'John', lastname: 'Doe', password: 'password', password_confirmation: 'password')
+  end
+
+  context 'with valid inputs' do
+    it 'stores user details and display user profile' do
+      visit login_path
+      # Fill in login details
+      fill_in 'Username', with: user.username
+      fill_in 'Password', with: 'password'
+      click_on 'Log In'
+      # Test data
+      expect(session[:user_id]).to eq(user.id)
+      # Test page
+      expect(page).to have_current_path(user_path(user))
+      expect(page).to have_content('Login successful')
+      expect(page).to have_content("Welcome #{user.firstname.capitalize}!")
+    end
+  end
+end
