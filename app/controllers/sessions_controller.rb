3f90b440b7c87c719065ee20026a82335e9580f3
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 17:08:42 +0800

    Add password validation as per #10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user 
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = 'Login successful'
       redirect_to user_path(user)
