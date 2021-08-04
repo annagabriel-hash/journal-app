74e9dbceaff7d99b72b3b2f4bed7978f6e8be0ce
Anna Gabriel <yssgabriel@gmail.com>
2021-08-04 16:19:48 +0800

    Add success message which completes and closes #8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     session[:user_id] = user.id
+    flash[:notice] = 'Login successful'
     redirect_to user_path(user)
   end
 end
