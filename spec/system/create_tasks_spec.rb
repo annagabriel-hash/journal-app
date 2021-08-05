86dba7a64839ad47a81044e1d2261db606e27d68
Edwin Sacalamitao <edwin@avionschool.com>
2021-08-05 19:37:52 +0800

    Updated to set category to optional when creating tasks as per annagabriel-hash/journal-app#14

--- spec/system/create_tasks_spec.rb
+++ spec/system/create_tasks_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "CreateTasks", type: :system do
+  let(:date) { DateTime.new(2021, 8, 4, 18, 24, 0)}
   before do
     driven_by(:rack_test)
   end
@@ -8,17 +9,17 @@ RSpec.describe "CreateTasks", type: :system do
   it 'saves and display task' do
     visit '/tasks/new'
     fill_in 'Todo', with: 'sample task'
-    fill_in 'Due', with: '2021-08-05 21:58:00 UTC'
-  fill_in 'Notes', with: 'sample notes'
-    click_on 'Submit task'
+    fill_in 'Due', with: date.strftime("%FT%R")
+    fill_in 'Notes', with: 'sample notes'
+    expect { click_on 'Submit task' }.to change(Task, :count).by(1)
 
     expect(page).to have_content('sample task')
-    expect(page).to have_content('2021-08-05 21:58:00 UTC')
+    expect(page).to have_content(date.strftime("%F %T UTC"))
     expect(page).to have_content('sample notes')
 
     task = Task.order("id").last
     expect(task.todo).to eq('sample task')
-    expect(task.due).to eq('2021-08-05 21:58:00 UTC')
+    expect(task.due).to eq(date)
     expect(task.notes).to eq('sample notes')
 
 
