c4accfe194bfc0d702cc92f4bfb92e218dcf830d
Anna Gabriel <yssgabriel@gmail.com>
2021-08-07 06:12:57 +0800

    Update task to nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#new'
-  resources :tasks
   get 'signup', to: 'users#new'
   resources :users, except: :new do
     resources :categories
+    resources :tasks
   end
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
