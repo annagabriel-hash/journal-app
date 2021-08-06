57f2092e5e7c93513d5829e5275ab03f50b4d84e
Edwin Sacalamitao <edwin@avionschool.com>
2021-08-07 05:22:31 +0800

    refactored task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: %i[ show edit update destroy]
 
     def index
         @tasks = Task.all
@@ -10,29 +11,30 @@ class TasksController < ApplicationController
 
     def create
         @task = Task.create(task_params)
+        redirect_to @task
     end
 
     def edit
-        @task = Task.find(params[:id])
     end
 
     def update
-        @task = Task.find(params[:id])
         @task.update! (task_params)
         redirect_to @task
     end
 
     def show
-        @task = Task.find(params[:id])
     end
 
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         redirect_to tasks_path, notice: 'Task was deleted successfully'
       end
 
     private
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
 
     def task_params
         params.require(:task).permit(:todo, :due, :notes)
